ff13bfb59e1137c4605fbb350a58417b3487b793
taliamo <talia.martz-oberlander@questu.ca>
2015-11-17 15:49:55 -0800

    saved nov/16 version

--- organ_pitch/Scripts/env_data.py
+++ organ_pitch/Scripts/env_data.py
@@ -1,5 +1,6 @@
 #T. Martz-Oberlander, 2015-11-15
 #Upload and clean environmental data
+# To run this script: $ python Scripts/env_data.py Data/[input filename] Figures/[output plot name] 
 
 #Import useful libraries
 import pandas as pd
@@ -9,12 +10,18 @@ import numpy as np
 
 %matplotlib inline
 
+#Define input arguments	
+input_file = sys.argv[1]
+output_plot_name = sys.argv[2]
+
+#define variables for the Cramer pitch calculation
+a0 = 331.5024
+a9 = -(-85.20931)
+a14 = 29.179762
+
+
 #I open my main function
 def main:
-
-#Define input arguments	
-	input_file = sys.argv[1]
-	#= sys.argv[2]
 
 #Upload environmental data file
 	env = pd.read_table(input_file, sep=',')
@@ -25,25 +32,29 @@ def main:
 #change data time variable to actual values of time. 
 	env['date_time']= pd.to_datetime(env['date_time'])
 
-#define variables for the pitch calculation
-a0 = 331.5024
-a14 = 29.179762
-
 #test function
-def test_cramer(data):
-        
-#pitch calculator function
+def test_cramer():
+    assert a0 + ((a9)*400)/100 + a14*((400/1000000)**2) == 672.339644669, 'Equation failure'
+    return()
+
+#Call the test function
+test_cramer()
+       
+#pitch calculator function from Cramer equation
 def cramer(data):
     '''Calculate pitch from CO2_1 concentration'''
-    #calc_pitch = []
-    calc_freq = a0 + a14*((data)**2)
- 
+    calc_freq = a0 + ((a9*data)/100) + a14*((data)**2) 
     return(calc_freq)
 
-#Run the function for the input column (CO2 values)
+#Run the function for the input column (CO2 values) to get a new column of calculated_frequency
 env['calc_freq'] = cramer(env['CO2_1'])
 
+#Import the measured pitch values--the output of pitch_data.py script
+pitch = pd.read_table('..data/pitch_data.csv, sep=',')
 
+
+
+#Function to make and save a plot
 
 
 #Close main function
--- organ_pitch/Scripts/pitch_data.py
+++ organ_pitch/Scripts/pitch_data.py
@@ -1,26 +1,24 @@
 #T. Martz-Oberlander, 2015-11-15
 #Script for wrangling pitch data into a dataframe with media and standard dev. of sound frequencies
-
+# To call this script: $ python Scripts/pitch_data.py Data/[input_filename] Data/[output_pitch_dataframe_name] 
 
 #Import useful libraries
 import pandas as pd
-import matplotlib.pyplot as plt
-import re
 import numpy as np
-
-%matplotlib inline
+import sys
 
 # I definte my arguments (input and output)
-input_file = sys.argv[1]
+input_filename = sys.argv[1]
 output_pitch_dataframe = sys.argv[2]
 
 #I open my main function
-def main:
+def main():
+
 #import pitch data file
-	pitch=pd.read_table(input_file, sep=',')
+	pitch=pd.read_table(input_filename, sep=',')
 
 #use date/time timestamp values
-	pitch['date_time']= pd.to_datetime(pitch['date_time'])
+	pitch['time']= pd.to_datetime(pitch['time'])
 
 #create new column for median frequency from 9 frequency measurements
 	pitch['median_freq'] = np.mean(pitch[['freq1','freq2','freq3', 'freq4', 'freq5', 'freq6', 'freq7', 'freq8', 'freq9']], axis=1)	
@@ -32,7 +30,7 @@ def main:
 	organized_pitch = pitch.groupby(['div']).get_group('choir')
 
 #save this dataframe as a file that can be called in later scripts
-	
+	organized_pitch.to_csv(output_pitch_dataframe, sep=',') 	
 
 #close main function
 main()
